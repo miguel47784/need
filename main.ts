03a3d914ba6e116d298f35c1b87a159ed3f9e888
miguel47784 <127358698+miguel47784@users.noreply.github.com>
2023-03-08 21:55:02 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
 basic.forever(function () {
-	
+    music.playMelody("E B C5 A B G A F ", 120)
+    basic.showIcon(IconNames.Happy)
+    basic.showString("Sauve")
 })
